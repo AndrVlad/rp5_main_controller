7e99e92f0e1e8a3d66614b951bd9abc750eff56a
Andrianooo <vladosandrianov@yandex.ru>
2026-08-03 12:38:12 +0300

    Исправил ошибку в расчете

--- 2.cpp
+++ 2.cpp
@@ -625,7 +625,7 @@ float get_battery_voltage() {
 
     /* ONLY FOR DEBUG */
     //sup_voltage = 12.1;
-    sup_voltage = std::trunc(sup_voltage* 10^2) / 10^2;
+    sup_voltage = std::trunc(sup_voltage* 100) / 100;
     return sup_voltage;
 }
 
